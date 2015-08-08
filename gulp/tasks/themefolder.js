119c3fbe5ead05ebc88ed56cc388dbb971ec4628
msorensson <martin.sorensson@creuna.se>
2015-08-08 23:15:35 +0200

    added themefolder:cleanup task to gulp

--- gulp/tasks/themefolder.js
+++ gulp/tasks/themefolder.js
@@ -1,6 +1,17 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    del = require('del');
 
-gulp.task('themefolder', function() {
+gulp.task('themefolder:cleanup', function() {
+    del([
+        './build/**/*',
+        '!./build/js',
+        '!./build/css',
+        '!./build/fonts',
+        '!./build/img'
+    ]);
+});
+
+gulp.task('themefolder', ['themefolder:cleanup'], function() {
     return gulp.src('./src/themefolder/**/*')
         .pipe(gulp.dest('./build'));
 });
